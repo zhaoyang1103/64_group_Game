c7c7ace20a92a89d64fadf439ac362551568223d
WangSen <765026374@qq.com>
2019-05-10 16:22:36 +0800

    上传项目说明

--- app/src/main/java/com/mad/trafficclient/ws_java/ob60/Subscribe_Main.java
+++ app/src/main/java/com/mad/trafficclient/ws_java/ob60/Subscribe_Main.java
@@ -9,17 +9,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.mad.trafficclient.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Go_Fight_Now on 2019/5/9 21:30
  */
 public class Subscribe_Main extends Fragment {
-    private ImageView ob60_img_add;
     private Context context;
+    private GridView ob60_toSubscribe;
+    private TextView ob60_toAdd;
+
+    private List<String> subscribeList;
+    private List<String> addList;
 
     @Nullable
     @Override
@@ -27,17 +36,90 @@ public class Subscribe_Main extends Fragment {
         View view = inflater.inflate(R.layout.ob60_main, container, false);
         initView(view);
         context = getContext();
+        subscribeList = new ArrayList<>();
+        addList = new ArrayList<>();
         return view;
     }
 
     private void initView(View view) {
-        ob60_img_add = (ImageView) view.findViewById(R.id.ob60_img_add);
+        ob60_toSubscribe = (GridView) view.findViewById(R.id.ob60_toSubscribe);
+        ob60_toAdd = (TextView) view.findViewById(R.id.ob60_toAdd);
+    }
 
-        ob60_img_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "框内数据", Toast.LENGTH_SHORT).show();
+
+    class SubscribeAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return subscribeList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.ob60_subscribe_item, parent, false);
+
+            return convertView;
+        }
+
+        public class ViewHolder {
+            public View rootView;
+            public TextView ob60_subscribe_text;
+            public ImageView ob60_grid_remove;
+
+            public ViewHolder(View rootView) {
+                this.rootView = rootView;
+                this.ob60_subscribe_text = (TextView) rootView.findViewById(R.id.ob60_subscribe_text);
+                this.ob60_grid_remove = (ImageView) rootView.findViewById(R.id.ob60_grid_remove);
             }
-        });
+
+        }
+    }
+
+    class AddAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return addList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.ob60_add_item, parent, false);
+
+            return convertView;
+        }
+
+        public class ViewHolder {
+            public View rootView;
+            public TextView ob60_add_text;
+            public ImageView ob60_grid_add;
+
+            public ViewHolder(View rootView) {
+                this.rootView = rootView;
+                this.ob60_add_text = (TextView) rootView.findViewById(R.id.ob60_add_text);
+                this.ob60_grid_add = (ImageView) rootView.findViewById(R.id.ob60_grid_add);
+            }
+
+        }
     }
 }
